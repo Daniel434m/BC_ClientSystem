206b946f1b8ec44277de7506b16521d1d143b9ad
Daniel434m <danielmuteka434@gmail.com>
2025-06-22 19:55:25 +0200

    Commit prior to Scaffolding of Controllers and Views, Entity Framework in place.

---
+++ BC_ClientSystem/DAL/ClientContext.cs
@@ -0,0 +1,30 @@
+﻿using BC_ClientSystem.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BC_ClientSystem.DAL
+{
+    public class ClientContext : DbContext
+    {
+        // ✅ Constructor pointing to "DefaultConnection"
+        public ClientContext() : base("ClientContext")
+        {
+        }
+
+        // ✅ Use DbSet<T> instead of ISet<T> 
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
+
+
+
+
+
+
+
